2ddc12d9720ba0cc993dea3cd0df46f70832ee09
Saul Romero <daw12.2025.jesuitas@gmail.com>
2025-12-10 11:54:00 +0100

    añadir admin

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,26 @@
+@php
+    use Illuminate\Support\Facades\Auth;
+@endphp
+    <!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Diseño Change.org Admin - Bootstrap</title>
+    <link rel="stylesheet" href="{{asset("css/styles.css")}}">
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant:wght@400;700&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+
+</head>
+
+<body>
+
+</body>
+
+</html>
